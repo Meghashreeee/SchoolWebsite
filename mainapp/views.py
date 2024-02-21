43f3a8e09693a2b11e40e898b65660d6970d8f6f
Manoj <manojpatil9147@gmail.com>
2024-02-21 11:54:19 +0530

    updated number of images loading

--- mainapp/views.py
+++ mainapp/views.py
@@ -11,7 +11,7 @@ app_name = 'mainapp'
 
 def home(request):
     path = settings.MEDIA_ROOT
-    img_list = random.sample(os.listdir(path + "/School_images/"),7)
+    img_list = random.sample(os.listdir(path + "/School_Images/"),7)
     values = Content.objects.values()
     Announce = Announcement.objects.values()
     faculty_list = faculty.objects.values()
